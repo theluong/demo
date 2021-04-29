064f5dc2fbb3f6f2def291224aa11ee83fe6bbcb
Luong Nguyen <nguyentheluong90@gmail.com>
2021-04-29 19:47:56 +0700

    mess

---
+++ JStest.js
@@ -0,0 +1,35 @@
+// *****JavaScrip*****JS/Jquery 
+// -viết trong thẻ HTML or css
+// -<scrip> </scrip>
+// -các kiểu dữ liệu
+// - DOM-> Element ->HTML
+//document.getelementbyid / .getelementbyclassname / .getelementbytagname
+//document.queryselector / all
+// var (noScpo)- let(scpo) - const ()
+// String- number-bolean
+//object - array
+//place, substring, random, 
+//prototype()khuôn mẫu
+//json
+
+
+//innertext khác innerhtml : 1 cái in hết text , 1 cái sẽ dạng HTML
+// array
+//for if else 
+//switch case
+//toan tu 
+//cau sự kiện true: fail  =  data>7 ? a : b
+//cai đặt sự kiện : click, focus, active, blur, key down, key up, key press, change
+
+//array: lenght, index, array[ư], cách nhau dấu phẩy, chuyển số thành chuỗi +"1", push(), shift(), pop(), splice(), 
+        // concat(), unshift(), split(), reserve()
+        // reduce 
+//createElement
+
+//nguyen tac code : SOLID, DRY
+
+//bootrap có opset để chia cột lẻ
+//responsive các mức độ màn hình đt, ipad, laptop
+
+//tao hieu ứng: transla-> opacity mờ dần
+
